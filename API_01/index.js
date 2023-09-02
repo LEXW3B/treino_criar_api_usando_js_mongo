83ddea3e6eab720fd242c08bac6489e293d47ad7
Ale <alexmanson12345@gmail.com>
2023-09-02 13:51:59 -0300

    feat: criado a rota para ver todos os usuários

--- API_01/index.js
+++ API_01/index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const users = [
+  "João Jatobá", "José Joaquim", "Patricia Pimentel", "Maria Madalena"
+];
+
 app.get('/', (_req, res) => {
   res.send('Hello World!')
 })
+
+// READ ALL
+app.get('/users', (_req, res) => {
+  res.status(201).send(users);
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
