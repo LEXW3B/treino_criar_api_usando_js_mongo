81078e47a4a39da773797c19f5ff9112874d4a34
Ale <alexmanson12345@gmail.com>
2023-09-02 16:13:14 -0300

    feat: configuração do mongoDB

--- API_01/index.js
+++ API_01/index.js
@@ -1,59 +1,75 @@
 const express = require('express')
-const app = express()
-app.use(express.json());
-const port = 3000
+const { MongoClient, ClientEncryption } = require('mongodb');
 
+// CONF MONGO
+const url = 'mongodb://localhost:27017';
+const nameDB = 'API_01';
+const client = new MongoClient(url);
 
-const users = [
-  "João Jatobá", "José Joaquim", "Patricia Pimentel", "Maria Madalena"
-];
+async function main() {
+  console.info('Conectando ao banco de dados...');
+  await client.connect();
+  console.info('Conectado com sucesso!');
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+  const bd = client.db(nameDB);
+  const collection = bd.collection('users');
 
-// READ ALL
-app.get('/users', (_req, res) => {
-  res.status(201).send(users);
-});
+  const app = express()
+  app.use(express.json());
+  const port = 3000
 
-// READ ONE
-app.get('/users/:id', (req, res) => {
-  const id = req.params.id;
-  const user = users[id - 1];
+  const users = [
+    "João Jatobá", "José Joaquim", "Patricia Pimentel", "Maria Madalena"
+  ];
 
-  if (!user) {
-    res.status(404).send({ message: 'User not found' });
-  };
+  app.get('/', (_req, res) => {
+    res.send('Hello World!')
+  })
 
-  res.status(200).send(user);
-});
+  // READ ALL
+  app.get('/users', (_req, res) => {
+    res.status(201).send(users);
+  });
 
-// CREATE
-app.post('/users', (req, res) => {
-  const { name } = req.body;
-  users.push(name);
+  // READ ONE
+  app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id - 1];
 
-  res.status(201).send({ message: 'User created successfully' });
-});
+    if (!user) {
+      res.status(404).send({ message: 'User not found' });
+    };
 
-// UPDATE
-app.put('/users/:id', (req, res) => {
-  const { id } =req.params;
-  const {  name } = req.body;
-  users[id - 1] = name;
+    res.status(200).send(user);
+  });
 
-  res.status(200).send({  message: 'User updated successfully' });
-});
+  // CREATE
+  app.post('/users', (req, res) => {
+    const { name } = req.body;
+    users.push(name);
 
-// DELETE
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  delete users[id - 1];
+    res.status(201).send({ message: 'User created successfully' });
+  });
 
-  res.status(200).send({ message: 'User deleted successfully'});
-});
+  // UPDATE
+  app.put('/users/:id', (req, res) => {
+    const { id } =req.params;
+    const {  name } = req.body;
+    users[id - 1] = name;
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`)
-})
+    res.status(200).send({  message: 'User updated successfully' });
+  });
+
+  // DELETE
+  app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    delete users[id - 1];
+
+    res.status(200).send({ message: 'User deleted successfully'});
+  });
+
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+  });
+}
+main();
