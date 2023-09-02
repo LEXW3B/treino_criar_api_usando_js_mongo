2db5a52b610495efbfb0f269dd110118d31df52f
Ale <alexmanson12345@gmail.com>
2023-09-02 14:05:42 -0300

    feat: criado a rota para ver um usuário

--- API_01/index.js
+++ API_01/index.js
@@ -16,6 +16,18 @@ app.get('/users', (_req, res) => {
   res.status(201).send(users);
 });
 
+// READ ONE
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const user = users[id - 1];
+
+  if (!user) {
+    res.status(404).send({ message: 'User not found' });
+  };
+
+  res.status(200).send(user);
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
 })
