9802d64cce658ff592dddfebd4f73d3ac7a9bcd5
Ale <alexmanson12345@gmail.com>
2023-09-11 17:43:12 -0300

    feat: criado middleware com um função de validPassword

--- API_05/middleware/authMiddleware.js
+++ API_05/middleware/authMiddleware.js
@@ -1,18 +1,34 @@
 function validEmail(req, res, next) {
   const { email } = req.body;
-  const REGEX = /^[a-zA-Z]{3,}@.*\..*$/;
+  const REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   if (email === undefined || email === '' || email === null) {
     res.status(400).json({ message: 'Invalid email' });
-  } else if (email < 3) {
+  } else if (email.length < 3) {
     res.status(400).json({ message: 'Invalid email' });
-  } else if (REGEX.test(email)) {
-    next();
+  } else if (!REGEX.test(email)) {
+    res.status(400).json({ message: 'Invalid email' });
   } else {
-    res.status(400).json({ message: 'Invalid email' });
+    next();
+  }
+}
+
+function validPassword(req, res, next) {
+  const { password } = req.body;
+  const REGEX = /^[a-zA-Z0-9]{6,}$/;
+
+  if (password === undefined || password === '' || password === null) {
+    res.status(400).json({ message: 'Invalid password 1' });
+  } else if (password.length < 6) {
+    res.status(400).json({ message: 'Invalid password 2' });
+  } else if (!REGEX.test(password)) {
+    res.status(400).json({ message: 'Invalid password 3' });
+  } else {
+    next();
   }
 }
 
 module.exports = {
   validEmail,
+  validPassword,
 };
--- API_05/routers/route.login.js
+++ API_05/routers/route.login.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const loginController = require('../app/controller/login.controller');
-const { validEmail } = require('../middleware/authMiddleware');
+const { validEmail, validPassword } = require('../middleware/authMiddleware');
 
 router.get('/', (_req, res) => {
   res.send("i'm me, and you who is?");
 });
 
 router.get('/login', loginController.getAll);
-router.post('/login', validEmail, loginController.create);
+router.post('/login', validEmail, validPassword, loginController.create);
 
 module.exports = router;
